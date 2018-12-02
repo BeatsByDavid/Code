ff1c9920984101b62c62c6f17c1f0380fe6b43ce
David Kopala <kopala.david@gmail.com>
2018-12-02 16:21:15 -0700

    Added some uploading stuff

---
+++ MiddleLayer/UploadingQueries.py
@@ -0,0 +1,33 @@
+from sql_declaratives import *
+from config import DB_STRING
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+class UploadingQueries:
+
+    # Start some SQL Alchemy stuff
+    def __init__(self):
+        self.engine = create_engine(DB_STRING)
+        Base.metadata.bind = self.engine
+        self.db_session = sessionmaker()
+        self.db_session.bind = self.engine
+
+    # Create a connection to the DB
+    def generate_session(self):
+        # type: () -> Session
+        return self.db_session()
+
+
+    def add_data(self, **kwargs):
+        # type: (dict) -> Data
+        session = self.generate_session()
+
+        new_item = Data(**kwargs)
+        session.add(new_item)
+        session.commit()
+
+        session.close()
+
+        return new_item
--- MiddleLayer/celery_tasks.py
+++ MiddleLayer/celery_tasks.py
@@ -3,10 +3,12 @@ import time
 from celery import Celery
 from flask_socketio import SocketIO
 
+from UploadingQueries import UploadingQueries
+
 app = Celery('bbd_tasks', backend='rpc://', borker='pyamqp://guest@localhost//')
 socketio = SocketIO(message_queue='amqp:///socketio')
 
-class CeleryTasks():
+class CeleryTasks:
 
     @app.task
     def long_task(n):
@@ -23,3 +25,14 @@ class CeleryTasks():
             time.sleep(1)
         socketio.emit('event', message, namespace='/namespace')
         print 'Sent: \'{0}\''.format(message)
+
+    def upload_new_data(**kwargs):
+        CeleryTasks.upload_new_data.delay(**kwargs)
+        return "OK"
+
+    @app.task
+    def upload_new_data_cel(**kwargs):
+        u = UploadingQueries()
+        new = u.add_data(**kwargs)
+        socketio.emit('new_data', new.to_json(), '/namespace')
+        print 'Added new data via celery task!'
