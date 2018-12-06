99e50839764e79ffbf52dc46cf25b3f4abb8a11c
johnson29 <31078470+johnson29@users.noreply.github.com>
2018-12-05 18:23:07 -0700

    Add files via upload

---
+++ Frontend/hello.py
@@ -0,0 +1,44 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+import json
+import requests
+ 
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+ 
+def getExchangeRates():
+    temp = []
+    sound = []
+    new_params = {
+  "id": "Postman Testing",
+  "method": "down.query_data",
+  "params": {
+    "limit":20,
+    "order_by":"timestamp",
+    "direction":"DESC"
+  }
+  }
+    response = requests.post('http://davidkopala.com:8000/api',json.dumps(new_params))
+    
+    data = response.text
+    print (data)
+    rdata = json.loads(data, parse_float=float)
+    for j in rdata['result']['raw']:
+      if j['type'] == 'Temperature':
+        temp.append(j['value'])
+      if j['type'] == 'Sound':
+        sound.append(float(j['value'])*100)
+    return temp , sound
+ 
+@app.route("/")
+def index():
+    temp , sound = getExchangeRates()
+    return render_template('test.html',**locals())      
+ 
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+ 
+ 
+if __name__ == "__main__":
+    app.run()
