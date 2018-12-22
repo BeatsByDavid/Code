3e46015bce0818141f02b12a9a71b4c3acad80c2
David Kopala <david.kopala@colorado.edu>
2018-12-21 22:21:48 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,2 +1,19 @@
+import requests
+import json
+
 print 'Testing Python File!'
+
+data = {
+        "id": "Postman Testing",
+        "method": "down.query_data",
+        "params": {
+                "limit":1,
+                "order_by":"id",
+                "direction":"DESC"
+        }
+    }
+
+r = requests.post('http://127.0.0.1:8000/api', json.dumps(data))
+json.loads(r.text)
+
 exit(0)
